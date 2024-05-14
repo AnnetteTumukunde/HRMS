dbcaf4526b7eebbf9de38f3dda3c9f4957c0c712
annettet <atumukunde@wys.rw>
2024-05-14 11:40:38 +0200

    Pushing Employee project

---
+++ Models/Designation.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Policy;
+
+namespace EmployeesManagement.Models
+{
+    public class Designation:UserActivity
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+
+    }
+}
